f18d5afbbba59380175723c40f15df4365fb5521
Son-le654 <simhtd21@gmail.com>
2023-07-05 18:08:18 +0700

    Update api

--- Climate_PE/src/main/java/com/example/demo/controller/AppointmentController.java
+++ Climate_PE/src/main/java/com/example/demo/controller/AppointmentController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,13 +72,12 @@ public class AppointmentController {
 	public List<Appointment> getAll() {
 		return appointmentService.findAll();
 	}
-	
+
 	@GetMapping("/listBypaintedId")
 	public List<Appointment> getByPaintedId(@RequestParam("painted_id") String paintedId) {
-	    return appointmentService.findByPaintedId(paintedId);
+		return appointmentService.findByPaintedId(paintedId);
 	}
 
-	
 	@GetMapping("/listIncome")
 	public List<Appointment> getAllIncome() {
 		return appointmentService.findAllNotApprove();
@@ -89,6 +90,37 @@ public class AppointmentController {
 			return ResponseEntity.ok().body(appointment.get());
 		} else {
 			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/commandFlag")
+	public ResponseEntity<String> updateCommandFlag(@RequestParam("appointmentId") int appointmentId,
+			@RequestParam("command") String command) {
+		try {
+			// Get the appointment by its ID
+			Optional<Appointment> optionalAppointment = appointmentService.findById(appointmentId);
+
+			if (optionalAppointment.isPresent()) {
+				Appointment appointment = optionalAppointment.get();
+
+				// Update the commandFlag based on the input
+				if (command.equalsIgnoreCase("cancel")) {
+					appointment.setCommandFlag(2);
+				} else if (command.equalsIgnoreCase("approve")) {
+					appointment.setCommandFlag(1);
+				} else {
+					return ResponseEntity.badRequest().body("Invalid command.");
+				}
+
+				// Save the updated appointment
+				appointmentService.saveAppointment(appointment);
+
+				return ResponseEntity.ok("CommandFlag updated successfully.");
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
 		}
 	}
 }
