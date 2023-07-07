0fdd99f8961ccfe120d8f67642f347febf001a34
Son-le654 <simhtd21@gmail.com>
2023-07-07 23:48:52 +0700

    fix google login

--- Climate_PE/src/main/java/com/example/demo/DTO/LoginRequest.java
+++ Climate_PE/src/main/java/com/example/demo/DTO/LoginRequest.java
@@ -8,6 +8,7 @@ public class LoginRequest implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String name;
 	private String email;
 	private String password;
 	
@@ -23,8 +24,15 @@ public class LoginRequest implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public LoginRequest(String email, String password) {
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LoginRequest(String name, String email, String password) {
 		super();
+		this.name = name;
 		this.email = email;
 		this.password = password;
 	}
