8c978dd552efd14526e095cd858167449ffe8ca2
Son-le654 <simhtd21@gmail.com>
2023-07-30 01:10:32 +0700

    Update frontend

--- src/module/admin/Content/AdminContentCurveChart.js
+++ src/module/admin/Content/AdminContentCurveChart.js
@@ -1,21 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Pie } from "@ant-design/plots";
+import axios from "axios";
+import { publicPort } from "components/url/link";
 
 const AdminContentCurveChart = () => {
-  const data = [
+  const [data, setData] = useState([
     {
       type: "Online",
-      value: 50,
+      value: 33.33,
     },
     {
       type: "Completed",
-      value: 25,
+      value: 33.33,
     },
     {
       type: "Cancel",
-      value: 25,
+      value: 33.33,
     },
-  ];
+  ]);
+  useEffect(() => {
+    // Fetch data from the API
+    axios.get(publicPort + "api/count-in-current-month")
+      .then(response => {
+        const { online, completed, cancel } = response.data;
+        // Update the state with the new data
+        setData([
+          {
+            type: "Online",
+            value: online,
+          },
+          {
+            type: "Completed",
+            value: completed,
+          },
+          {
+            type: "Cancel",
+            value: cancel,
+          },
+        ]);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
   const config = {
     appendPadding: 10,
     data,
