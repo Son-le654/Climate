8922d22ce0469b8f077dbad240be14c83a24a05f
Phong Nguyen <nguyendinhphong1842001@gmail.com>
2023-08-04 17:19:44 +0700

    Update code

--- src/module/home/ContentService/SolutionContentDoctor.js
+++ src/module/home/ContentService/SolutionContentDoctor.js
@@ -3,7 +3,7 @@ import { BsArrowRightShort, BsCalendar3 } from "react-icons/bs";
 import { HiOutlineDocumentText } from "react-icons/hi";
 import { useState } from "react";
 
-function SolutionContent() {
+function SolutionContentDoctor() {
     const [hoveritem, setHoverItem] = useState("first")
 
     const hoverActive = (item) => {
@@ -23,7 +23,7 @@ function SolutionContent() {
                                 <span className="text-[35px] w-[15%] h-[40px] bg-[#eaf7f0] flex justify-center items-center rounded-full text-[#40b772]"><HiUserGroup /></span>
                             </div>
                             <div className="w-[100%] h-[50px] flex items-center">
-                                <h1 className="text-[20px] font-bold">Dashboard</h1>
+                                <h1 className="text-[20px] font-bold">Find Doctor</h1>
                             </div>
                             <div className="w-[100%] h-[130px] ">
                                 <p className="text-[15px] w-[75%]">The search feature in the clinic management application allows users to search and select doctors based on specific criteria.</p>
@@ -42,7 +42,7 @@ function SolutionContent() {
                                 <span className="text-[35px] w-[15%] h-[40px] bg-[#e4e5ed] flex justify-center items-center rounded-full text-[#efac72]"><BsCalendar3 /></span>
                             </div>
                             <div className="w-[100%] h-[50px] flex items-center">
-                                <h1 className="text-[20px] font-bold">Internal</h1>
+                                <h1 className="text-[20px] font-bold">Appointment</h1>
                             </div>
                             <div className="w-[100%] h-[130px] ">
                                 <p className="text-[15px] w-[75%]">The appointment management feature in the clinic management application allows users and staff to efficiently manage appointments.</p>
@@ -61,7 +61,7 @@ function SolutionContent() {
                                 <span className="text-[35px] w-[15%] h-[40px] bg-[#e3dfef] flex justify-center items-center rounded-full text-[#eb5757]"><HiOutlineDocumentText /></span>
                             </div>
                             <div className="w-[100%] h-[50px] flex items-center">
-                                <h1 className="text-[20px] font-bold">Account</h1>
+                                <h1 className="text-[20px] font-bold">Submit Requests</h1>
                             </div>
                             <div className="w-[100%] h-[130px] ">
                                 <p className="text-[15px] w-[75%]">Sending requests in the clinic management application allows users and patients to send requests to staff or doctors within the clinic.</p>
@@ -77,4 +77,4 @@ function SolutionContent() {
         </div >
     )
 }
-export default SolutionContent
+export default SolutionContentDoctor
--- src/module/home/HomeContentServiceStaff.js
+++ src/module/home/HomeContentServiceStaff.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import imgDoctor from "../../Images/Doctor2.png";
+import imgDoctor from "../../Images/imgAdmin.png";
 import HomeDoctorLookupSevirce from "./part/HomeDoctorLookupSevirce";
 import InformationCatalogueService from "./part/InformationCatalogueService";
 import { useNavigate } from "react-router-dom";
@@ -8,6 +8,8 @@ import LatestNewService from "./part/LatestNewService";
 import { publicPort } from "../../components/url/link";
 import jwtDecode from "jwt-decode";
 import SolutionContent from "./ContentService/SolutionContent";
+import SolutionContentDoctor from "./ContentService/SolutionContentDoctor";
+import SolutionContentNurse from "./ContentService/SolutionContentNurse";
 const HomeContentServiceStaff = () => {
   const tabButtons = ["In-Person", "E - consultations"];
   const [type, setType] = useState("In-Person");
@@ -141,9 +143,9 @@ const HomeContentServiceStaff = () => {
           {role == "ADMIN" ?
             <SolutionContent />
             : role == "DOCTOR" ?
-              <></>
+              <SolutionContentDoctor />
               :
-              <></>
+              <SolutionContentNurse />
           }
         </div>
       </div>
@@ -171,7 +173,7 @@ const HomeContentServiceStaff = () => {
           <LatestNewService />
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
